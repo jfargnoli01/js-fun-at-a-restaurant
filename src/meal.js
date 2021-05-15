e1e1e0cef56f10484842d80917f1601f65a765d7
Juliana Fargnoli <fargnoli.jm@gmail.com>
2021-05-15 17:26:52 -0600

    Initial Commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
+function nameMenuItem(menuItem) {
+  return "Delicious " + menuItem;
+};
 
-}
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+};
 
+function addIngredients(ingredient, ingredients) {
+  //create a for loop for ingredients array
+  for (var i = 0; i < ingredients.length; i++) {
+    //if any of the ingredients indecies exists already
+    if (ingredients[i] === ingredient) {
+      // dont add ingredient
+      return
+    }
+  }
+  ingredients.push(ingredient);
+};
 
+function formatPrice(price) {
+  return `$${price}`;
+};
+
+function decreasePrice(price) {
+  return price * .9;
+};
+
+function createRecipe(title, ingredients, menuItemType) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  };
+};
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe,
 }
-
-
