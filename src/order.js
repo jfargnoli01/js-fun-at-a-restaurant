076eeb56719bb54f0eb46c578fbe96081ba5fae0
Juliana Fargnoli <fargnoli.jm@gmail.com>
2021-05-16 11:13:15 -0600

    Update order.js functions to pass tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(order);
+  }
+};
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (orderNumber === deliveryOrders[i].orderNumber) {
+      deliveryOrders.splice([i],1);
+    }
+  }
+};
+
+function listItems(deliveryOrders) {
+  //build array of items
+  var items = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+      items.push(deliveryOrders[i].item);
+  }
+  // merge strings in array
+  return items.join(', ');
+  // return string of items
+};
+
+function searchOrder(deliveryOrders, itemName) {
+  //create for loop to run through orders
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    //determine if order exists
+    if (deliveryOrders[i].item === itemName) {
+      return true;
+    }
+  }
+  // item not found in loop, return false
+  return false;
+};
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
-}
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
+};
